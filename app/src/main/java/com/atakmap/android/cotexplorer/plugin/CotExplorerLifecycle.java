abbb847ee41cd9a22b15439177a5f53eb803b2fd
Charlie Benbow <cfbenbow@gmail.com>
2024-12-26 17:20:33 -0500

    Updating CotExplorerDropDownReceiver and CotExplorerMapComponent class names.

--- app/src/main/java/com/atakmap/android/cotexplorer/plugin/CotExplorerLifecycle.java
+++ app/src/main/java/com/atakmap/android/cotexplorer/plugin/CotExplorerLifecycle.java
@@ -6,12 +6,13 @@ import com.atak.plugins.impl.PluginContextProvider;
 
 import gov.tak.api.plugin.IPlugin;
 import gov.tak.api.plugin.IServiceController;
-import com.atakmap.android.cotexplorer.cotexplorerMapComponent;
+
+import com.atakmap.android.cotexplorer.CotExplorerMapComponent;
 
 public class CotExplorerLifecycle extends AbstractPlugin implements IPlugin {
 
     public CotExplorerLifecycle(IServiceController serviceController) {
-        super(serviceController, new CotExplorerTool(serviceController.getService(PluginContextProvider.class).getPluginContext()), new cotexplorerMapComponent());
+        super(serviceController, new CotExplorerTool(serviceController.getService(PluginContextProvider.class).getPluginContext()), new CotExplorerMapComponent());
     }
 }
 
