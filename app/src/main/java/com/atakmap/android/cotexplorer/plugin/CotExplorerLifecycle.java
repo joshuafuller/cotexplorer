620e4a7271ff62c41cec441fd35f544aac9bea57
Charlie Benbow <cfbenbow@gmail.com>
2024-12-26 17:08:52 -0500

    Updating CotExplorerTool and cot_explorer drawable.

---
+++ app/src/main/java/com/atakmap/android/cotexplorer/plugin/CotExplorerLifecycle.java
@@ -0,0 +1,17 @@
+
+package com.atakmap.android.cotexplorer.plugin;
+
+import com.atak.plugins.impl.AbstractPlugin;
+import com.atak.plugins.impl.PluginContextProvider;
+
+import gov.tak.api.plugin.IPlugin;
+import gov.tak.api.plugin.IServiceController;
+import com.atakmap.android.cotexplorer.cotexplorerMapComponent;
+
+public class CotExplorerLifecycle extends AbstractPlugin implements IPlugin {
+
+    public CotExplorerLifecycle(IServiceController serviceController) {
+        super(serviceController, new CotExplorerTool(serviceController.getService(PluginContextProvider.class).getPluginContext()), new cotexplorerMapComponent());
+    }
+}
+
