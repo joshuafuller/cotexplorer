73635604eb2e3e1b75a4dd74dd1cf4856b571bb7
SLAB-8002 <Rsa!Tryf.zHCd6NTpivBjW4xfftgg@@KqngZaHdEPryk28*!>
2025-07-09 18:17:00 -0400

    Version submitted to TAK TPP.

---
+++ app/build/generated/source/buildConfig/civ/release/com/atakmap/android/cotexplorer/plugin/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.atakmap.android.cotexplorer.plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.atakmap.android.cotexplorer.plugin";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "civ";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "2.0 (null) - [5.4.0]";
+  // Field from the variant API
+  public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
+}
